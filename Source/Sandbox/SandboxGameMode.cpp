33d37b181420977fa3d8ebffedd457c967687d6e
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2018-02-11 21:53:35 -0500

    Initial commit

---
+++ Source/Sandbox/SandboxGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "SandboxGameMode.h"
+#include "SandboxHUD.h"
+#include "SandboxCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ASandboxGameMode::ASandboxGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ASandboxHUD::StaticClass();
+}
